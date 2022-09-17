bea77ca46807b6cff83c797a38012bc8b29b4603
Jerald Velicaria <69361447+jcveli@users.noreply.github.com>
2022-09-16 22:20:02 -0700

    added cart context

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,24 @@
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
+import { CartContext } from '../../context/cart.context'
+import { useContext } from 'react'
 
 const CartDropdown = () => {
-    
+    const { cartItems } = useContext(CartContext);
+
+
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items' />
+        <div className='cart-items'>
+            {cartItems.length ? (
+            cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))
+            ) : (
+            <span className='empty-message'>Your cart is empty</span>
+            )}
+        </div>
             <Button>Go To Checkout</Button>
         </div>
     )
